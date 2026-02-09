6622f9ccbd0bea63636ab4d184532847da65a894
Vaibhav Shukla <vaibhavshukla565@gmail.com>
2026-02-09 15:14:47 +0530

    fix image bug

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
       <div className="row text-center">
         <img
           src={homeHero}
-          alt="Hero Image"
+          alt="hero"
           className="mb-3 mb-md-5 img-fluid"
         />
         <h1 className="mt-3 mt-md-5 fs-2 fs-md-1">Invest in everything</h1>
